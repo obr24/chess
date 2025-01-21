1f4f0fc9aa64b54ee118933621dae50bc2fe68c2
Orion <obr24@byu.edu>
2025-01-21 16:58:50 -0700

    Update QueenMovesCalculator.java to pass all tests

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,4 +1,17 @@
 package chess;
 
-public class QueenMovesCalculator {
+import java.util.Collection;
+import chess.BishopMovesCalculator;
+import chess.RookMovesCalculator;
+
+public class QueenMovesCalculator implements PieceMovesCalculator {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        BishopMovesCalculator bishopPiece = new BishopMovesCalculator();
+        RookMovesCalculator rookPiece = new RookMovesCalculator();
+
+        Collection<ChessMove> queenMoves = bishopPiece.pieceMoves(board, position);
+        queenMoves.addAll(rookPiece.pieceMoves(board, position));
+        return queenMoves;
+    }
+
 }
