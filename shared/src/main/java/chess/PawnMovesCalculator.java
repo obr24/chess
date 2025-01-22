66eefa574a51930d7591748826a6a6899660ceec
Orion <obr24@byu.edu>
2025-01-22 16:15:22 -0700

    Pass most white pawn tests

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -3,6 +3,8 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static chess.ChessPiece.PieceType;
+
 public class PawnMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> pawnMoves = new ArrayList<>();
@@ -14,23 +16,36 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
         if (currPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
             ChessPosition aheadPiecePosition = new ChessPosition(row + 1, col);
             ChessPiece aheadPiece = board.getPiece(aheadPiecePosition);
-            ChessPiece aheadLeftPiece = board.getPiece(new ChessPosition(row, col - 1)); // TODO check if at left side
-            ChessPiece aheadRightPiece = board.getPiece(new ChessPosition(row, col + 1)); // TODO check if at right side
+
+            ChessPosition aheadLeftPiecePosition = new ChessPosition(row + 1, col - 1);
+            ChessPiece aheadLeftPiece = board.getPiece(aheadLeftPiecePosition); // TODO check if at left side
+
+            ChessPosition aheadRightPiecePosition = new ChessPosition(row + 1, col + 1);
+            ChessPiece aheadRightPiece = board.getPiece(aheadRightPiecePosition); // TODO check if at right side
+
             if (row == 2) {     // If in starting position
                 ChessPosition twoAheadPiecePosition = new ChessPosition(row + 2, col);
                 ChessPiece twoAheadPiece = board.getPiece(twoAheadPiecePosition);
-                if (aheadPiece == null) {
-                    pawnMoves.add(new ChessMove(position, aheadPiecePosition, null));
-                }
                 if (twoAheadPiece == null) {
                     pawnMoves.add(new ChessMove(position, twoAheadPiecePosition, null));
                 }
-            } else if (row < 7) {   // If not in promotion range
+            }
+            if (row < 7) {   // If not in promotion range
                 if (aheadPiece == null) {
-                    pawnMoves.add(new ChessMove(position, new ChessPosition(row + 1, col), null));
+                    pawnMoves.add(new ChessMove(position, aheadPiecePosition, null));
+                }
+                if (aheadLeftPiece != null && aheadLeftPiece.getTeamColor() != currPiece.getTeamColor()) {
+                    pawnMoves.add(new ChessMove(position, aheadLeftPiecePosition, null));
+                }
+                if (aheadRightPiece != null && aheadRightPiece.getTeamColor() != currPiece.getTeamColor()) {
+                    pawnMoves.add(new ChessMove(position, aheadRightPiecePosition, null));
                 }
             } else {        // If in promotion range
-
+                if (aheadPiece == null) { // todo also check for if right and left sides are empty?
+                    for(PieceType possiblePiece : new PieceType[] {PieceType.QUEEN, PieceType.BISHOP, PieceType.KNIGHT, PieceType.ROOK}) {
+                        pawnMoves.add(new ChessMove(position, aheadPiecePosition, possiblePiece));
+                    }
+                }
             }
         }
         return pawnMoves;
