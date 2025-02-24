93369f72776467f69518a68304a04e29c665ab97
Orion <obr24@byu.edu>
2025-02-24 10:11:20 -0700

    Update all files for first start for phase 3

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        var newServer = new Server();
+        newServer.run(8080);
     }
 }
