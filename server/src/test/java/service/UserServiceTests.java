136a215101997e46236685969cfdf82bead92e66
Orion <obr24@byu.edu>
2025-03-01 23:06:07 -0700

    add test file - doens't work

---
+++ server/src/test/java/service/UserServiceTests.java
@@ -0,0 +1,35 @@
+package java.service;
+
+import dataaccess.*;
+import model.RequestsAndResults;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import service.ServiceException;
+
+import java.util.Objects;
+
+import static service.UserService.register;
+
+public class UserServiceTests {
+
+    UserDAO memoryUserDAO = new MemoryUserDAO();
+    AuthDAO memoryAuthDAO = new MemoryAuthDAO();
+    GamesDAO memoryGamesDAO = new MemoryGamesDAO();
+
+    @Test
+    @Order(1)
+    @DisplayName("register")
+    public void registerTest() {
+        RequestsAndResults.RegisterResult registerResult = null;
+        try {
+            registerResult = register(memoryUserDAO, memoryAuthDAO,
+                    new RequestsAndResults.RegisterRequest("u", "p", "e"));
+        }
+        catch (ServiceException e) {
+            Assertions.assertNull(e);
+        }
+        Assertions.assertNotNull(registerResult);
+    }
+}
