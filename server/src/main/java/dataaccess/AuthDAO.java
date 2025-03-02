3e360a0a62596a1c14b7d276c2c4b8dd12ccd6f5
Orion <obr24@byu.edu>
2025-03-01 17:13:09 -0700

    Update classes to pass register tests

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,11 @@
+package dataaccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    void createAuth(AuthData authData) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+}
+
+
+
