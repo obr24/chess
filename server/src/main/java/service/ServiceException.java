bfe014af9150ee64773a2ad7c0edb56e2e749b7e
Orion <obr24@byu.edu>
2025-03-01 17:17:06 -0700

    Update ServiceException for Register tests

--- server/src/main/java/service/ServiceException.java
+++ server/src/main/java/service/ServiceException.java
@@ -1,7 +1,24 @@
 package service;
 
-public class ServiceException extends RuntimeException {
-  public ServiceException(String message) {
-    super(message);
-  }
+import com.google.gson.Gson;
+import spark.Response;
+
+import java.util.HashMap;
+
+public class ServiceException extends Exception {
+  private int responseCode;
+    public ServiceException(String message, int responseCode) {
+        super(message);
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+      return responseCode;
+    }
+
+    public HashMap<String, String> getResponse() {
+        HashMap<String, String> responseHashmap = new HashMap<>();
+        responseHashmap.put("message", getMessage());
+        return responseHashmap;
+    }
 }
