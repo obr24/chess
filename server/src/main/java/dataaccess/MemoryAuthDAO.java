a74d92a97ec7c7df4b4a8dbfe019d3468d09371c
Orion <obr24@byu.edu>
2025-03-01 17:13:59 -0700

    Update MemoryAuthDAO to pass register tests

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,22 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private Collection<AuthData> auths = new ArrayList<>();
+
+    @Override
+    public void createAuth(AuthData authData) throws DataAccessException {
+        AuthData newAuth = new AuthData(authData.authToken(), authData.username());
+        this.auths.add(newAuth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        throw new DataAccessException("implement it");
+    }
 }
