43ba1f6eb8319629df7dbcadebffcd5168d6a261
Orion <obr24@byu.edu>
2025-03-01 17:50:50 -0700

    Update server/src/main/java/dataaccess/UserDAO.java to pass login tests

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -6,4 +6,5 @@ public interface UserDAO {
     void createUser(UserData user) throws DataAccessException;
     UserData getUser(String username) throws DataAccessException;
     void reset() throws DataAccessException;
+    boolean validUser(String username, String password);
 }
