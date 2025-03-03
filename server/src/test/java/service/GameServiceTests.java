1d25527b8ba7a94806c269e3a136c8d69047964b
Orion <obr24@byu.edu>
2025-03-03 14:04:37 -0700

    Add 2 more gameservice tests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -51,7 +51,7 @@ public class GameServiceTests {
     }
 
     @Test
-    @Order(4)
+    @Order(3)
     @DisplayName("joinGame- good request")
     public void createGameGoodTest() {
         String authToken = null;
@@ -74,6 +74,57 @@ public class GameServiceTests {
         } catch (ServiceException e) {
 
         }
+    }
 
+    @Test
+    @Order(4)
+    @DisplayName("joinGame- good request1")
+    public void createGameGoodTest1() {
+        String authToken = null;
+        int gameID = 0;
+        try {
+            authToken = UserService.register(memoryUserDAO, memoryAuthDAO,
+                    new RequestsAndResults.RegisterRequest(
+                            "username1", "password1", "email1")).authToken();
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+        try {
+            gameID = GameService.createGame(memoryGamesDAO, memoryAuthDAO, "", "gameName1").gameID();
+            Assertions.assertNotEquals(0, gameID);
+        } catch (ServiceException e) {
+            Assertions.assertNotNull(e);
+        }
+        try {
+            GameService.joinGame(memoryUserDAO, memoryGamesDAO, memoryAuthDAO, authToken, "WHITE", gameID);
+        } catch (ServiceException e) {
+
+        }
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("joinGame- good request2")
+    public void createGameGoodTest2() {
+        String authToken = null;
+        int gameID = 0;
+        try {
+            authToken = UserService.register(memoryUserDAO, memoryAuthDAO,
+                    new RequestsAndResults.RegisterRequest(
+                            "username1", "password1", "email1")).authToken();
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+        try {
+            gameID = GameService.createGame(memoryGamesDAO, memoryAuthDAO, "", "gameName1").gameID();
+            Assertions.assertNotEquals(0, gameID);
+        } catch (ServiceException e) {
+            Assertions.assertNotNull(e);
+        }
+        try {
+            GameService.joinGame(memoryUserDAO, memoryGamesDAO, memoryAuthDAO, authToken, "WHITE", gameID);
+        } catch (ServiceException e) {
+
+        }
     }
 }
