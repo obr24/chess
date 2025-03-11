d1730bd98e5fc635c9352016578db6b89ea5a593
Orion <obr24@byu.edu>
2025-03-10 19:38:26 -0600

    add new test

--- server/src/test/java/dataaccess/userDaoTests.java
+++ server/src/test/java/dataaccess/userDaoTests.java
@@ -1,20 +1,15 @@
 package dataaccess;
 
+import dataaccess.MemoryUserDAO;
 import dataaccess.SqlUserDAO;
 import dataaccess.UserDAO;
-import model.RequestsAndResults;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import service.GameService;
-import service.ServiceException;
-import service.UserService;
 
-public class DAOtests {
+public class userDaoTests {
     UserDAO memoryUserDAO = new MemoryUserDAO();
-    AuthDAO memoryAuthDAO = new MemoryAuthDAO();
-    GamesDAO memoryGamesDAO = new MemoryGamesDAO();
 
     @Test
     @Order(1)
