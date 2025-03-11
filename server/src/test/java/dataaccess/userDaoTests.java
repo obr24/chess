407c2fce0ddb61d038263f8b0ebaaf65af35a4a5
Orion <obr24@byu.edu>
2025-03-10 19:47:06 -0600

    add another few tests

--- server/src/test/java/dataaccess/userDaoTests.java
+++ server/src/test/java/dataaccess/userDaoTests.java
@@ -3,6 +3,9 @@ package dataaccess;
 import dataaccess.MemoryUserDAO;
 import dataaccess.SqlUserDAO;
 import dataaccess.UserDAO;
+import dataaccess.GamesDAO;
+
+import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
@@ -21,4 +24,55 @@ public class userDaoTests {
             Assertions.assertNull(e);
         }
     }
+
+    @Test
+    @Order(2)
+    @DisplayName("New game dao")
+    public void createGameDAOTest() {
+        try {
+            GamesDAO gamesDAO = new SqlGamesDAO();
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("New auth dao")
+    public void createAuthDAOTest() {
+        try {
+            AuthDAO authDAO = new SqlAuthDAO();
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Create user successfully")
+    public void createUserSuccessTest() {
+        try {
+            UserDAO userDAO = new SqlUserDAO();
+            UserData testUser = new UserData("testUser", "securePassword123", "test@example.com");
+
+            userDAO.createUser(testUser);
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Create user with duplicate username")
+    public void createUserDuplicateTest() {
+        try {
+            UserDAO userDAO = new SqlUserDAO();
+            UserData testUser = new UserData("", "", "");
+
+            userDAO.createUser(testUser); // First insert (should succeed)
+        } catch (Exception e) {
+            Assertions.fail("Unexpected exception: " + e.getMessage());
+        }
+    }
+
 }
