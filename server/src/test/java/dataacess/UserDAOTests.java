02a140bea8c382cb18975a6027da86c32e6be119
Orion <obr24@byu.edu>
2025-03-10 19:06:27 -0600

    Add new test

--- server/src/test/java/dataacess/UserDAOTests.java
+++ server/src/test/java/dataacess/UserDAOTests.java
@@ -1,4 +1,26 @@
-package java.dataacess;
+package dataacess;
+
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import model.RequestsAndResults;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import service.GameService;
+import service.ServiceException;
+import service.UserService;
 
 public class UserDAOTests {
+
+    @Test
+    @Order(1)
+    @DisplayName("create db")
+    public void createDBTest() {
+        try {
+            UserDAO memoryUserDAO = new MemoryUserDAO();
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+    }
 }
