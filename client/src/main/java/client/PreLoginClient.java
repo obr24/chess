89a83776d9ef7cf868292deba52f09b98195c64c
Orion <obr24@byu.edu>
2025-03-24 22:40:48 -0600

    Basic repl register functionality added

--- client/src/main/java/client/PreLoginClient.java
+++ client/src/main/java/client/PreLoginClient.java
@@ -1,12 +1,63 @@
 package client;
 
+import exception.ResponseException;
+import model.RequestsAndResults;
+
+import java.util.Arrays;
+
 public class PreLoginClient {
-    private final ServerFacade server;
+    private final ServerFacade facade;
     private final String serverUrl;
 
     public PreLoginClient(String serverUrl) {
-        server = new ServerFacade(serverUrl);
+        facade = new ServerFacade(serverUrl);
         this.serverUrl = serverUrl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "login" -> login(params);
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                case "help" -> help();
+                default -> unknownInput();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String login(String... params) throws ResponseException {
+        return "login not implemented";
+//        if (params.length >= 1) {
+//            state = State.SIGNEDIN;
+//            visitorName = String.join("-", params);
+//            ws = new WebSocketFacade(serverUrl, notificationHandler);
+//            ws.enterPetShop(visitorName);
+//            return String.format("You signed in as %s.", visitorName);
+//        }
+//        throw new ResponseException(400, "Expected: <yourname>");
+    }
+
+    public String register(String... params) throws ResponseException {
+        if (params.length == 3) {
+            RequestsAndResults.RegisterResult registerResult;
+            try {
+                registerResult = facade.register(new RequestsAndResults.RegisterRequest(params[0], params[1], params[2]));
+            } catch (ResponseException e) {
+                throw new ResponseException(400, e.getMessage());
+            }
+//            state = State.SIGNEDIN;
+//            visitorName = String.join("-", params);
+//            ws = new WebSocketFacade(serverUrl, notificationHandler);
+//            ws.enterPetShop(visitorName);
+            return String.format("You registered as %s.", params[0]);
+        }
+        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD> <EMAIL>");
     }
 
     public String help() {
@@ -18,5 +69,9 @@ public class PreLoginClient {
                 Exit: "quit"
                 Help: "help"
                 """;
+    }
+
+    public String unknownInput() {
+        return String.format("Unknown input:\n%s", help());
     }
 }
--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,5 +1,8 @@
 package client;
 
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class Repl {
     // each repl is sort of nested in the other. ie. A user starts in client.PreLoginClient. Then they login and
     // go to client.PostLoginClient. Then they choose a game and go to the inGameCleint.
@@ -8,11 +11,40 @@ public class Repl {
     private final PreLoginClient client;
 
     public Repl(String serverUrl) {
-        client = new PreLoginClient(serverUrl);
+        client = new PreLoginClient("http://127.0.0.1");
+        // todo next time make sure to start server before starting client
+        // u need to work on the preloginclient, finsih that, then set the logged in state to true
+        // then create post loginclient
+        // then after joinging game, call post join client
+        // then print the board. good luck
     }
 
     public void run() {
         System.out.println("Welcome to Chess. Sign in to start.");
         System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Exception e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" +  resetAll() + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+    private String resetAll() {
+        return String.format(RESET_TEXT_COLOR);
     }
 }
