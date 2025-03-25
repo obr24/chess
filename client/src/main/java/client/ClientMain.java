77a7c08eb63e04e7df13125965df8105c056e3ea
Orion <obr24@byu.edu>
2025-03-25 14:38:32 -0600

    Update now passes tests. file: client/src/main/java/client/ClientMain.java

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -6,7 +6,7 @@ public class ClientMain {
     public static void main(String[] args) {
         // todo what should the serverUrl be?
 
-        var serverUrl = "http://localhost:8080";
+        var serverUrl = "http://127.0.0.1:8080";
         if (args.length == 1) {
             serverUrl = args[0];
         }
