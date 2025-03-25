6f17afb29bace6734404123c286fd455f7e4a368
Orion <obr24@byu.edu>
2025-03-24 21:48:34 -0600

    Create new facade instance

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -18,6 +18,7 @@ public class ServerFacadeTests {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(String.format("http://127.0.0.1:%s", port));
     }
 
     @AfterAll
