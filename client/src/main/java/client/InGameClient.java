91f38208e78ad170551e256c2442417b3186d566
Orion <obr24@byu.edu>
2025-03-25 14:38:32 -0600

    Update now passes tests. file: client/src/main/java/client/InGameClient.java

--- client/src/main/java/client/InGameClient.java
+++ client/src/main/java/client/InGameClient.java
@@ -1,4 +1,15 @@
 package client;
 
 public class InGameClient {
+    private final ServerFacade facade;
+    private final ClientState clientState;
+
+    public InGameClient(ServerFacade facade, ClientState clientState) {
+        this.facade = facade;
+        this.clientState = clientState;
+    }
+
+    public String eval(String input) {
+    return "not implemented: in game client";
+    }
 }
