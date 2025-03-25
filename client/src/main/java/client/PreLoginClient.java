7f8b283fbeac01884776e0b163890ca8da4f9b4c
Orion <obr24@byu.edu>
2025-03-25 14:38:32 -0600

    Update now passes tests. file: client/src/main/java/client/PreLoginClient.java

--- client/src/main/java/client/PreLoginClient.java
+++ client/src/main/java/client/PreLoginClient.java
@@ -1,17 +1,21 @@
 package client;
 
+import chess.ChessGame;
 import exception.ResponseException;
+import model.GameData;
 import model.RequestsAndResults;
+import ui.PrintBoard;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class PreLoginClient {
     private final ServerFacade facade;
-    private final String serverUrl;
+    private final ClientState clientState;
 
-    public PreLoginClient(String serverUrl) {
-        facade = new ServerFacade(serverUrl);
-        this.serverUrl = serverUrl;
+    public PreLoginClient(ServerFacade facade, ClientState clientState) {
+        this.facade = facade;
+        this.clientState = clientState;
     }
 
     public String eval(String input) {
@@ -24,23 +28,38 @@ public class PreLoginClient {
                 case "register" -> register(params);
                 case "quit" -> "quit";
                 case "help" -> help();
+                // todo remove:
+                case "b" -> boardTest();
+                case "v" -> boardTestBlack();
                 default -> unknownInput();
             };
         } catch (ResponseException ex) {
-            return ex.getMessage();
+            return String.format("error in prelogin: %s", ex.getMessage());
         }
     }
 
+    // todo: remove this function
+    public String boardTest() {
+        return new PrintBoard().print(new GameData(999, "whiteUs", "blk", "gme",
+                new ChessGame()), "observer");
+    }
+    public String boardTestBlack() {
+        return new PrintBoard().print(new GameData(999, "whiteUs", "blk", "gme",
+                new ChessGame()), "black");
+    }
     public String login(String... params) throws ResponseException {
-        return "login not implemented";
-//        if (params.length >= 1) {
-//            state = State.SIGNEDIN;
-//            visitorName = String.join("-", params);
-//            ws = new WebSocketFacade(serverUrl, notificationHandler);
-//            ws.enterPetShop(visitorName);
-//            return String.format("You signed in as %s.", visitorName);
-//        }
-//        throw new ResponseException(400, "Expected: <yourname>");
+        if (params.length == 2) {
+            RequestsAndResults.LoginResult loginResult;
+            try {
+                loginResult = facade.login(new RequestsAndResults.LoginRequest(params[0], params[1]));
+                clientState.setUserState(State.POST_LOGIN);
+                clientState.setAuthToken(loginResult.authToken());
+            } catch (ResponseException e) {
+                throw new ResponseException(400, String.format("error in register: %s", e.getMessage()));
+            }
+            return String.format("You logged in as %s.", params[0]);
+        }
+        throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD>");
     }
 
     public String register(String... params) throws ResponseException {
@@ -48,13 +67,11 @@ public class PreLoginClient {
             RequestsAndResults.RegisterResult registerResult;
             try {
                 registerResult = facade.register(new RequestsAndResults.RegisterRequest(params[0], params[1], params[2]));
+                clientState.setUserState(State.POST_LOGIN);
+                clientState.setAuthToken(registerResult.authToken());
             } catch (ResponseException e) {
-                throw new ResponseException(400, e.getMessage());
+                throw new ResponseException(400, String.format("error in register: %s", e.getMessage()));
             }
-//            state = State.SIGNEDIN;
-//            visitorName = String.join("-", params);
-//            ws = new WebSocketFacade(serverUrl, notificationHandler);
-//            ws.enterPetShop(visitorName);
             return String.format("You registered as %s.", params[0]);
         }
         throw new ResponseException(400, "Expected: <USERNAME> <PASSWORD> <EMAIL>");
