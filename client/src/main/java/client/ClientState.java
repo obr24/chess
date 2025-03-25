24ad5f8a13ed8d05c089794449876b268abd0031
Orion <obr24@byu.edu>
2025-03-25 14:38:33 -0600

    Update now passes tests. file: client/src/main/java/client/ClientState.java

---
+++ client/src/main/java/client/ClientState.java
@@ -0,0 +1,72 @@
+package client;
+
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class ClientState {
+    public ChessGame.TeamColor getPlayerColor() {
+        return playerColor;
+    }
+
+    public void setPlayerColor(ChessGame.TeamColor playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    private ChessGame.TeamColor playerColor;
+    public Integer getObservingGameID() {
+        return observingGameID;
+    }
+
+    public void setObservingGameID(Integer observingGameID) {
+        this.observingGameID = observingGameID;
+    }
+
+    private Integer observingGameID;
+    private HashMap<Integer, GameData> clientGameIdtoRealGameId;
+
+    public Integer getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(Integer gameID) {
+        this.gameID = gameID;
+    }
+
+    private Integer gameID;
+
+    public HashMap<Integer, GameData> getClientGameIdtoRealGameId() {
+        return clientGameIdtoRealGameId;
+    }
+
+    public void setClientGameIdtoRealGameId(HashMap<Integer, GameData> clientGameIdtoRealGameId) {
+        this.clientGameIdtoRealGameId = clientGameIdtoRealGameId;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    private String authToken;
+
+    public State getUserState() {
+        return userState;
+    }
+
+    public void setUserState(State userState) {
+        this.userState = userState;
+    }
+
+    private State userState;
+
+    public ClientState() {
+        userState = State.PRE_LOGIN;
+    }
+}
+
