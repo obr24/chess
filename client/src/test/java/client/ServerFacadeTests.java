1f11d0775839abc0241876b7edf684cf7bbdbe3b
Orion <obr24@byu.edu>
2025-03-24 21:48:22 -0600

    Add import of ServerFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
