401e2a1417e042852a6c690ae63e9ff4558eb79d
Orion <obr24@byu.edu>
2025-04-07 10:34:41 -0600

    Comment out unnecessary code

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -24,3 +24,28 @@ public class WebSocketHandler {
 //            case EXIT -> exit(action.visitorName());
 //        }
     }
+
+//    private void enter(String visitorName, Session session) throws IOException {
+//        connections.add(visitorName, session);
+//        var message = String.format("%s is in the shop", visitorName);
+//        var notification = new Notification(Notification.Type.ARRIVAL, message);
+//        connections.broadcast(visitorName, notification);
+//    }
+
+//    private void exit(String visitorName) throws IOException {
+//        connections.remove(visitorName);
+//        var message = String.format("%s left the shop", visitorName);
+//        var notification = new Notification(Notification.Type.DEPARTURE, message);
+//        connections.broadcast(visitorName, notification);
+//    }
+
+//    public void makeNoise(String petName, String sound) throws ResponseException {
+//        try {
+//            var message = String.format("%s says %s", petName, sound);
+//            var notification = new Notification(Notification.Type.NOISE, message);
+//            connections.broadcast("", notification);
+//        } catch (Exception ex) {
+//            throw new ResponseException(500, ex.getMessage());
+//        }
+//    }
+}
