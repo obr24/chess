4d1d7bb219310b10abae017c0536a505617b31a9
Orion <obr24@byu.edu>
2025-04-07 15:46:36 -0600

    Update connectionmanager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,7 +1,7 @@
 package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
-import webSocketMessages.Notification;
+//import messages.ServerMessage;
 
 import java.io.IOException;
 import java.util.ArrayList;
