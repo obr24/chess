6767fa8eaf98d9e188f1d25b1231703a73f51445
Orion <obr24@byu.edu>
2025-04-07 17:00:31 -0600

    Update websocket for new functions

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -10,8 +10,8 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
 
-    public void add(String username, Session session) {
-        var connection = new Connection(username, session);
+    public void add(String username, Integer gameID, Session session) {
+        var connection = new Connection(username, gameID, session);
         connections.put(username, connection);
     }
 
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,29 +1,57 @@
 package server.websocket;
 
 import com.google.gson.Gson;
+import dataaccess.*;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import service.GameService;
+import service.ServiceException;
 import websocket.commands.UserGameCommand;
+import service.GameService.*;
 import websocket.messages.ServerMessage;
+
+import static chess.ChessGame.TeamColor.WHITE;
+import static service.GameService.joinGame;
 
 @WebSocket
 public class WebSocketHandler {
+    private ConnectionManager connections = new ConnectionManager();
 
-    private final ConnectionManager connections = new ConnectionManager();
+    private GamesDAO gamesDAO = null;
+    private UserDAO userDAO = null;
+    private AuthDAO authDAO = null;
+
+    public WebSocketHandler() {
+        connections = new ConnectionManager();
+        try {
+            gamesDAO = new SqlGamesDAO();
+            userDAO = new SqlUserDAO();
+            authDAO = new SqlAuthDAO();
+        } catch (Exception e) {
+            System.out.printf("Error in websockethandler: %s%n", e.getMessage());
+        }
+    }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
         System.out.printf("Received: %s", message);
         UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
         switch (command.getCommandType()) {
-            case CONNECT -> connect(command.getAuthToken(), command.getGameID());
+            case CONNECT -> connect(session, command.getAuthToken(), command.getGameID());
             case null, default -> unknown();
         }
     }
 
-    private void connect(String authToken, Integer gameID) {
-        return;
+    private void connect(Session session, String authToken, Integer gameID) {
+        try {
+            connections.add(authDAO.getAuth(authToken).username(), gameID, session);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        ConnectionManager.send
+        session.getRemote().sendString(msg);
     }
 
     private void unknown() {
