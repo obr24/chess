bcc7e19440306b580aaa20e7ef79151985109587
Orion <obr24@byu.edu>
2025-04-08 21:17:29 -0600

    Resign, leave now working in WebSocketFacade. file: client/src/main/java/client/websocket/NotificationHandler.java

--- client/src/main/java/client/websocket/NotificationHandler.java
+++ client/src/main/java/client/websocket/NotificationHandler.java
@@ -1,4 +1,16 @@
 package client.websocket;
 
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
 public interface NotificationHandler {
+    void notify(ServerMessage message);
+
+    void notifyError(ErrorMessage errorMessage);
+
+    void notifyNotification(NotificationMessage notificationMessage);
+
+    void notifyLoadGame(LoadGameMessage loadGameMessage);
 }
